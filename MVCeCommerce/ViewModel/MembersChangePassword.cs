41d5589c36216ae65d3258e661f85f362a02981c
3A732087 <quannn0180716v@gmail.com>
2022-01-17 20:58:55 +0800

    ViewModel建立(Members)

---
+++ MVCeCommerce/ViewModel/MembersChangePassword.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCeCommerce.ViewModel
+{
+    public class MembersChangePassword
+    {
+        [DisplayName("舊密碼")]
+        [Required(ErrorMessage = "請輸入舊密碼")]
+        public string Password { get; set; }
+
+        [DisplayName("新密碼")]
+        [Required(ErrorMessage = "請輸入新密碼")]
+        public string NewPassword { get; set; }
+
+        [DisplayName("新密碼確認")]
+        [Required(ErrorMessage = "請輸入確認密碼")]
+        [Compare("NewPassword", ErrorMessage = "兩次密碼輸入不一致")]
+        public string NewPasswordCheck { get; set; }
+    }
+}
---
+++ MVCeCommerce/ViewModel/MembersRegisterViewModel.cs
@@ -0,0 +1,26 @@
+﻿using MVCeCommerce.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCeCommerce.ViewModel
+{
+    public class MembersRegisterViewModel
+    {
+        [DisplayName("大頭貼")]
+        public HttpPostedFileBase MembersImage { get; set; }
+
+        public Members newMember { get; set; }
+        [DisplayName("密碼")]
+        [Required(ErrorMessage = "請輸入密碼")]
+        public string Password { get; set; }
+
+        [DisplayName("確認密碼")]
+        [Compare("Password", ErrorMessage = "兩次密碼輸入不一致")]
+        [Required(ErrorMessage = "請輸入確認密碼")]
+        public string PasswordCheck { get; set; }
+    }
+}
