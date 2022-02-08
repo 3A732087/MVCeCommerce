915bf4ec1157c303eec55cbccd50f5881dd309dc
3A732087 <quannn0180716v@gmail.com>
2022-02-08 09:43:48 +0800

    ViewModel建立(ItemCreate, ItemDetail, Item)

---
+++ MVCeCommerce/ViewModel/ItemDetailViewModel.cs
@@ -0,0 +1,15 @@
+﻿using MVCeCommerce.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCeCommerce.ViewModel
+{
+    public class ItemDetailViewModel
+    {
+        public Item Data { get; set; }
+
+        public bool InCart { get; set; }
+    }
+}
---
+++ MVCeCommerce/ViewModel/ItemViewModel.cs
@@ -0,0 +1,21 @@
+﻿using MVCeCommerce.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCeCommerce.ViewModel
+{
+    public class ItemViewModel
+    {
+        //顯示資料陣列
+        public List<int> IdList { get; set; }
+
+        //商品區塊
+        public List<ItemDetailViewModel> ItemBlock { get; set; }
+
+        //分頁內容
+        public ForPaging Paging { get; set; }
+
+    }
+}
