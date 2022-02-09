3c8dbd46470e19abcbe44df39464367162eba5aa
3A732087 <quannn0180716v@gmail.com>
2022-02-09 21:53:10 +0800

    jQuery建立(Article, ArticleList, MessageList)

---
+++ MVCeCommerce/jQuery/Article/ArticleList.js
@@ -0,0 +1,9 @@
+﻿//畫面載入後事件
+$(function () {
+    //綁定新增文章按鈕點擊事件
+    //因新增文章按鈕是後來用ajax取回產生，故採用$(document).delegate
+
+    $(document).delegate('#CreateArticleModal #createBtn', 'click', function () {
+        $('#CreateArticleModal form').submit();
+    });
+});
