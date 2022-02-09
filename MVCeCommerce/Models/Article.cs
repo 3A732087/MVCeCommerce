ccece88c0d3530517defff450aef5c5fd9d4f414
3A732087 <quannn0180716v@gmail.com>
2022-02-09 09:25:26 +0800

    Article, Message Model驗證功能

--- MVCeCommerce/Models/Article.cs
+++ MVCeCommerce/Models/Article.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,16 +9,24 @@ namespace MVCeCommerce.Models
 {
     public class Article
     {
+        [DisplayName("文章編號")]
         public int A_Id { get; set; }
 
+        [DisplayName("標題")]
+        [Required(ErrorMessage = "請輸入標題")]
         public string Title { get; set; }
 
+        [DisplayName("文章內容")]
+        [Required(ErrorMessage = "請輸入文章內容")]
         public string Content { get; set; }
 
+        [DisplayName("發表者")]
         public string Account { get; set; }
 
+        [DisplayName("新增時間")]
         public DateTime CreateTime { get; set; }
 
+        [DisplayName("觀看數")]
         public int Watch { get; set; }
 
         public Members Member { get; set; } = new Members();
--- MVCeCommerce/Models/Message.cs
+++ MVCeCommerce/Models/Message.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Web;
 
@@ -11,10 +12,13 @@ namespace MVCeCommerce.Models
 
         public int M_Id { get; set; }
 
+        [DisplayName("留言帳號：")]
         public string Account { get; set; }
 
+        [DisplayName("留言內容：")]
         public string Content { get; set; }
 
+        [DisplayName("留言時間：")]
         public DateTime CreateTime { get; set; }
 
         public Members Member { get; set; } = new Members();
