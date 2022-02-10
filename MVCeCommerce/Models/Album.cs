d8c10ad64ced35a892e10ff8ff329aab9f4ab7c2
3A732087 <quannn0180716v@gmail.com>
2022-02-10 09:34:03 +0800

    ViewModel建立(Album)、Album Model DisplayName

--- MVCeCommerce/Models/Album.cs
+++ MVCeCommerce/Models/Album.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Web;
 
@@ -8,14 +9,26 @@ namespace MVCeCommerce.Models
     public class Album
     {
         //編號
+        [DisplayName("編號")]
         public int Alb_Id { get; set; }
+
         //檔名
+        [DisplayName("檔名")]
         public string FileName { get; set; }
+
+
         //路徑
+        [DisplayName("路徑")]
         public string Url { get; set; }
+
+        [DisplayName("大小（Byte）")]
         public int Size { get; set; }
         public string Type { get; set; }
+
+        [DisplayName("上傳者")]
         public string Account { get; set; }
+
+        [DisplayName("上傳時間")]
         public DateTime CreateTime { get; set; }
 
         //Members資料表(外來鍵)
---
+++ MVCeCommerce/ViewModel/AlbumViewModel.cs
@@ -0,0 +1,24 @@
+﻿using MVCeCommerce.Models;
+using MVCeCommerce.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCeCommerce.ViewModel
+{
+    public class AlbumViewModel
+    {
+        [DisplayName("上傳圖片")]
+        [FileExtensions(ErrorMessage = "所上傳檔案不是圖片")]
+        public HttpPostedFileBase upload { get; set; }
+
+        public List<Album> FileList { get; set; }
+
+        public ForPaging Paging { get; set; }
+
+        public Album File { get; set; }
+    }
+}
