50e6b9e7cc6c4419082857662b8c6465a768a9cb
3A732087 <quannn0180716v@gmail.com>
2022-02-11 10:58:34 +0800

    ViewModel建立(Home)

---
+++ MVCeCommerce/ViewModel/HomeViewModel.cs
@@ -0,0 +1,20 @@
+﻿using MVCeCommerce.Models;
+using MVCeCommerce.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace MVCeCommerce.ViewModel
+{
+    public class HomeViewModel
+    {
+        [DisplayName("搜尋")]
+        public string Search { get; set; }
+
+        public List<Members> DataList { get; set; }
+
+        public ForPaging Paging { get; set; }
+    }
+}
